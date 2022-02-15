3ca0273e2d79825388f48bb48bc4f3590efd1826
Dinesh C Pandey <dineshchandrapandey@in.ibm.com>
2022-02-15 12:01:09 +0530

    added Chart for cp4ba operator
    Signed-off-by: Dinesh C Pandey <dineshchandrapandey@in.ibm.com>

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ locals {
   service_url   = "http://${local.name}.${var.namespace}"
   values_content = {
   }
-  layer = "services"
+  layer = "services" 
   type  = "base"
   application_branch = "main"
   namespace = var.namespace
