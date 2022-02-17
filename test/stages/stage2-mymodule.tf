d3c0a1126bef49421fa36d85a616b14657fad5a1
Dinesh C Pandey <dineshchandrapandey@in.ibm.com>
2022-02-17 20:19:07 +0530

    changed the storage class definition
    Signed-off-by: Dinesh C Pandey <dineshchandrapandey@in.ibm.com>

--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -6,8 +6,8 @@ module "gitops_module" {
   server_name = module.gitops.server_name
   namespace = module.gitops_namespace.name
   kubeseal_cert = module.gitops.sealed_secrets_cert
-  #storage_class_gold_name=module.cp4a-file-retain-gold-gid.storage_name
-  #storage_class_silver_name=module.cp4a-file-retain-silver-gid.storage_name
-  #storage_class_bronze_name=module.cp4a-file-retain-bronze-gid.storage_name
-  storageclass_operator=module.cp4a-file-retain-gold-gid.storage_name
+  storage_class_gold_name=module.gitops-cp4a-file-retain-gold-gid.storage_name
+  storage_class_silver_name=module.gitops-cp4a-file-retain-silver-gid.storage_name
+  storage_class_bronze_name=module.gitops-cp4a-file-retain-bronze-gid.storage_name
+  storageclass_operator=module.gitops-cp4a-file-retain-gold-gid.storage_name
 }
