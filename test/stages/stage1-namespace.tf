9038edad22e3c67176aff05048a2a5c2431afb95
Dinesh C Pandey <dineshchandrapandey@in.ibm.com>
2022-04-11 20:16:53 +0530

    removed the default value for namespace
    Signed-off-by: Dinesh C Pandey <dineshchandrapandey@in.ibm.com>

--- test/stages/stage1-namespace.tf
+++ test/stages/stage1-namespace.tf
@@ -3,7 +3,9 @@ module "gitops_namespace" {
 
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
-  name = var.namespace
+  #name = var.namespace
+  name = "project-cp4ba"
+  
 }
 
 resource null_resource write_namespace {
